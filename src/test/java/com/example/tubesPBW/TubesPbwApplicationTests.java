ebe171c6dbcffd60145fb9910c80a6d41900b262
AzrielAldrinSenandoSiahaan <6182201070@student.unpar.ac.id>
2025-12-03 17:22:34 +0700

    Menambahkan build spring boot gradle

---
+++ src/test/java/com/example/tubesPBW/TubesPbwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tubesPBW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubesPbwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
